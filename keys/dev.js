5cd887e7b8f5442e225f256e1bb02d27493babf5
Sparsh Samir <sparshsamir1993@gmail.com>
2020-05-04 14:44:53 -0400

    Delete dev.js

--- keys/dev.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-    mongooseURI : "mongodb://sparshsamir:13111993b@ds239071.mlab.com:39071/pamper-me",
-    googleClientID: "876652858738-ivs36rsaog4ijd2vednd8hk4jeesqjvf.apps.googleusercontent.com",
-    googleClientSecret: "7g5xpubwOCLn4EMbGSM-WtSz",
-    cookieKey: "sparshsamir"
-}
