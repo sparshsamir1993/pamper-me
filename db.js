0bf7ab70f75d226de089430bb640aa37022e3cda
sparshsamir <sparshsamir1993@gmail.com>
2020-05-09 22:52:39 -0400

    database connection for production

--- db.js
+++ db.js
@@ -1,13 +1,28 @@
 const Sequelize = require("sequelize");
-const sequelize = new Sequelize("pamperTest","root", "password", { host: "localhost", dialect: "mysql", operatorsAliases: false});
+const config = require("./config/config.json");
+let sequelize;
+if (process.env.NODE_ENV === "development") {
+  sequelize = new Sequelize("pamperTest", "root", "password", {
+    host: "localhost",
+    dialect: "mysql",
+    operatorsAliases: false,
+  });
+} else if (process.env.NODE_ENV === "production") {
+  sequelize = new Sequelize(
+    process.env.DB_NAME,
+    process.env.DB_USERNAME,
+    process.env.DB_PASSWORD,
+    {
+      host: process.env.DB_HOST,
+      dialect: "mysql",
+      operatorsAliases: false,
+    }
+  );
+}
 
 module.exports = sequelize;
 
 global.sequelize = sequelize;
-
-
-
-
 
 // const mysql = require("mysql");
 
@@ -24,5 +39,3 @@ global.sequelize = sequelize;
 //     console.log('Connected to database');
 // });
 // module.exports = db;
-
-
